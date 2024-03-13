ba4d1d499514e0a4164e1498aa7898d60588f4f3
Ibrahim Hafez <ibrahimhafez1@yahoo.com>
2024-03-13 19:21:00 +0200

    add sorting and pagination

--- controllers/products.js
+++ controllers/products.js
@@ -1,11 +1,7 @@
 const Product = require('../models/product.js')
 
-const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({})
-    res.status(200).json({ products })
-}
 const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query
+    const { featured, company, name, sort, fields } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -18,11 +14,28 @@ const getAllProducts = async (req, res) => {
         queryObject.name = { $regex: name, $options: 'i' }
     }
 
-    const products = await Product.find(queryObject)
+    let result = Product.find(queryObject)
+    if (sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    }
+    else {
+        result = result.sort('createdAt')
+    }
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ')
+        result = result.select(fieldsList)
+    }
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+    result = result.skip(skip).limit(limit)
+
+    const products = await result
+
     res.status(200).json({ products })
 }
 
 module.exports = {
-    getAllProducts,
-    getAllProductsStatic
+    getAllProducts
 }
--- routes/products.js
+++ routes/products.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const router = express.Router();
 
-const { getAllProducts, getAllProductsStatic } = require('../controllers/products.js')
+const { getAllProducts } = require('../controllers/products.js')
 
 router.route('/').get(getAllProducts)
-router.route('/static').get(getAllProductsStatic)
 
 module.exports = router
